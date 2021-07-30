cd3f31e48cd8cac2aef50fa19164c2f8d9e61722
Filfimo Yulfiz Ahsanul Hulqi <58451478+4ahsanul@users.noreply.github.com>
2021-07-30 13:42:52 +0700

    File buat db SQL
    File SQL terbaru, tanpa tabel Kuis

--- quiz.sql
+++ quiz.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.3
+-- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 30 Jul 2021 pada 02.08
--- Versi server: 10.4.14-MariaDB
--- Versi PHP: 7.4.11
+-- Generation Time: Jul 30, 2021 at 08:41 AM
+-- Server version: 10.4.20-MariaDB
+-- PHP Version: 7.3.29
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,7 +24,7 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `guru`
+-- Table structure for table `guru`
 --
 
 CREATE TABLE `guru` (
@@ -37,45 +37,21 @@ CREATE TABLE `guru` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data untuk tabel `guru`
+-- Dumping data for table `guru`
 --
 
 INSERT INTO `guru` (`id`, `nama`, `jk`, `noHp`, `username`, `password`) VALUES
 ('  101', '  Rahayu', '  Perempuan', '  08776', '  rahayu1@mail', '123'),
 ('  123', '  Priya', '  Laki-Laki', '  082313782937', 'priya@gmail.ecom  ', '456'),
 ('102', 'Agung', 'Laki-Laki', '0821353124153', 'agung', '321'),
-('12346', 'Yanuarso  ', 'Laki-Laki', '0857324', 'ekoyanu@mail', '123'),
 ('12366', 'Joko', 'Laki-laki', '08776', 'joko123@mail', '123'),
-('coba', 'coba', 'L', '0776', 'ekocoba@mail', '123'),
-('fix', 'fix', 'fix', 'fix  ', 'fix  ', 'fix');
+('222', 'Hadi', 'Laki-Laki', '231341', 'dadi', '222'),
+('433', 'Hani', 'Perempuan', '3819287328', 'hani', '433');
 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `kuis`
---
-
-CREATE TABLE `kuis` (
-  `kodekuis` varchar(50) NOT NULL,
-  `judulkuis` varchar(255) NOT NULL,
-  `mapel` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data untuk tabel `kuis`
---
-
-INSERT INTO `kuis` (`kodekuis`, `judulkuis`, `mapel`) VALUES
-('1234', 'Quiz1', 'Matematika'),
-('54643', 'coba2', 'Matematika'),
-('89077', 'Coba', 'Bahasa Jawa'),
-('BI115', 'Quiz1', 'Bahasa Indonesia'),
-('SB2', 'Quiz2', 'Seni Budaya dan Keterampilan');
-
--- --------------------------------------------------------
-
---
--- Struktur dari tabel `question`
+-- Table structure for table `question`
 --
 
 CREATE TABLE `question` (
@@ -90,25 +66,26 @@ CREATE TABLE `question` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data untuk tabel `question`
+-- Dumping data for table `question`
 --
 
 INSERT INTO `question` (`id`, `mapel`, `soal`, `optA`, `optB`, `optC`, `optD`, `answer`) VALUES
 (1, 'Matematika', 'Salah satu metode Matematika', 'Mamalia', 'Subtitusi', 'Ovipar', 'Ovovivipar', 'Subtitusi'),
-(2, 'Matematika', '1+2', '3', '4', '5', '2', '3'),
-(3, 'Matematika', '25x3', '45', '50', '75', '65', '75'),
-(4, 'Matematika', '1+1', '3', '4', '5', '2', '2'),
-(5, 'Matematika', '2x2', '2', '1', '3', '4', '4'),
-(6, 'Matematika', '5+5', '5', '6', '7', '10', '10'),
-(7, 'Matematika', '7x2', '8', '10', '14', '24', '14'),
-(8, 'Matematika', '23-2', '20', '21', '22', '23', '21'),
-(9, 'Matematika', '3x3', '6', '9', '2', '1', '9'),
-(10, 'Matematika', '10-x = 7', '1', '2', '3', '4', '3');
+(2, 'Matematika', 'Hasil dari 15 x 50 ÷ 30 adalah', '25', '35', '45', '55', '25'),
+(3, 'Matematika', 'Hasil dari 70 – (–25) adalah', '–95', '–45', '45', '95', '95'),
+(4, 'Matematika', 'Hasil dari 200 km + 15 hm – 21.000 m adalah…m', '180.500', '181.680', '182.366', '183.658', '180.500'),
+(5, 'Matematika', 'Bilangan yang hanya memiliki dua faktor disebut', 'Bilangan Asli', 'Bilangan Cacah', 'Bilangan Prima', 'Bilangan Ganjil', 'Bilangan Prima'),
+(6, 'Matematika', 'Bilangan yang bukan merupakan bilangan asli adalah', '23', '5', '119', '0', '0'),
+(7, 'Matematika', 'Lambang bilangan Romawi dari 739 adalah', 'DCXXXIX', 'DCCXXXIX', 'DCCXXVIIII', 'DCCXXXVIIII', 'DCCXXXIX'),
+(8, 'Matematika', 'Nilai dari MMXIV adalah', '2.014', '2.040', '2.140', '2.016', '2.014'),
+(9, 'Matematika', '-5+17', '-22', '-12', '12', '11', '12'),
+(10, 'Matematika', 'Hasil dari 11+ (-19) adalah', '30', '8', '-20', '-30', '-30'),
+(11, 'Matematika', '2+2', '1', '2', '3', '4', '4');
 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `siswa`
+-- Table structure for table `siswa`
 --
 
 CREATE TABLE `siswa` (
@@ -120,36 +97,31 @@ CREATE TABLE `siswa` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data untuk tabel `siswa`
+-- Dumping data for table `siswa`
 --
 
 INSERT INTO `siswa` (`nama`, `jk`, `nip`, `email`, `marks`) VALUES
-('Eko Yanuarso Budi', 'Laki', '1145', 'eko@mail', 90);
+('Eko Yanuarso Budi', 'Laki', '1145', 'eko@mail', 90),
+('Budi', 'Laki-Laki', '32314', 'bud@gmail.com', 80);
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indeks untuk tabel `guru`
+-- Indexes for table `guru`
 --
 ALTER TABLE `guru`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indeks untuk tabel `kuis`
---
-ALTER TABLE `kuis`
-  ADD PRIMARY KEY (`kodekuis`);
-
---
--- Indeks untuk tabel `question`
+-- Indexes for table `question`
 --
 ALTER TABLE `question`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indeks untuk tabel `siswa`
+-- Indexes for table `siswa`
 --
 ALTER TABLE `siswa`
   ADD PRIMARY KEY (`nip`);
